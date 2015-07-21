567b4290a261b62b9ae93680c2135c5bee29aaeb
malcolmmacleod <malcolm@macleodhome.com>
2015-07-21 14:53:33 +0100

    Additional changes for auto layout

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var secure = false {
+        didSet {
+            updateUI()
+        }
     }
+    
+    var loggedInUser : User? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        self.passwordField.secureTextEntry = secure
+        self.passwordLabel.text = secure ? "Secured Password" : "Password"
+        self.nameLabel.text = loggedInUser?.name
+        self.companyLabel.text = loggedInUser?.company
+        imageView.image = loggedInUser?.image
+    }
+    @IBAction func toggleSecurity() {
+        self.secure = !self.secure
+    }
+    
+    @IBAction func login() {
+        loggedInUser = User.login(self.loginField.text ?? "", password: self.passwordField.text ?? "")
+    }
+}
 
-
+extension User {
+    var image : UIImage? {
+        if let image = UIImage(named: login) {
+            return image
+        } else {
+            return UIImage (named: "unknown_user")
+        }
+    }
 }
 
